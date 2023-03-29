20dcc9bae717e54420faaed4f3e916e0ac52e728
Ksenia <kseniapopova02@gmail.com>
2023-03-29 15:39:42 +0200

    add: prettier and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,34 @@
-const path = require("path");
-const mode = process.env.NODE_ENV || "development";
-const devMode = mode === "development";
-const target = devMode ? "web" : "browserslist";
-const devtool = devMode ? "source-map" : undefined;
+const path = require('path');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+const target = devMode ? 'web' : 'browserslist';
+const devtool = devMode ? 'source-map' : undefined;
 
 module.exports = {
-  mode,
-  target,
-  devtool,
-  entry: ["@babel/polyfill", path.resolve(__dirname, "src", "index.js")],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-    filename: "index.[contenthash].js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [["@babel/preset-env", { targets: "defaults" }]],
-          },
-        },
-      },
-    ],
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+    mode,
+    target,
+    devtool,
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        clean: true,
+        filename: 'index.[contenthash].js',
     },
-  },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: 'defaults' }],
+                        ],
+                    },
+                },
+            },
+        ],
+    },
 };
